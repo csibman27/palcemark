597aff57310003a87024f2f8aeecd00dc4199a93
tibor molnar <tmolnair@gmail.com>
2023-02-16 19:10:24 +0000

    added partials, views, controllers and routes

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,7 +1,24 @@
+import { db } from "../models/db.js";
+
 export const dashboardController = {
   index: {
     handler: async function (request, h) {
-      return h.view("main");
+      const placemarks = await db.placemarkStore.getAllPlacemarks();
+      const viewData = {
+        title: "Placemark Dashboard",
+        placemarks: placemarks,
+      };
+      return h.view("dashboard-view", viewData);
+    },
+  },
+
+  addPlacemark: {
+    handler: async function (request, h) {
+      const newPlayList = {
+        title: request.payload.title,
+      };
+      await db.placemarkStore.addPlacemark(newPlacemark);
+      return h.redirect("/dashboard");
     },
   },
 };
