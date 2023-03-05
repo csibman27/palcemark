c8b18da0f539f512799c5107b0253de6502e68c4
tibor molnar <tmolnair@gmail.com>
2023-03-05 18:27:07 +0000

    more work done to finalise tests and implement api tests

---
+++ test/api/placemark-api-test.js
@@ -0,0 +1,17 @@
+import { assert } from "chai";
+import { playtimeService } from "./playtime-service.js";
+import { assertSubset } from "../test-utils.js";
+
+suite("Placemark API test", () => {
+  setup(async () => {});
+
+  teardown(async () => {});
+
+  test("create placemark", async () => {});
+
+  test("delete a placemark", async () => {});
+
+  test("create multiple placemarks", async () => {});
+
+  test("remove non-existant placemark", async () => {});
+});
