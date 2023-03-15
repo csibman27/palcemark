d0a23a91c7a8dff4194305d75918789bc1a1c211
tibor molnar <tmolnair@gmail.com>
2023-03-15 20:46:16 +0000

    fix credentials

--- src/controllers/about-controller.js
+++ src/controllers/about-controller.js
@@ -13,19 +13,4 @@ export const aboutController = {
       return h.view("about-view", viewData);
     },
   },
-  gmaps: {
-    handler: function initMap() {
-      const myLatLng = { lat: 52.2458777, lng: -7.138828 };
-      const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 14,
-        center: myLatLng,
-      });
-      new google.maps.Marker({
-        position: myLatLng,
-        map,
-        src: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBSH-MEAYrP70lwxXkW0_-bt1jq9uXKtjg&callback=initMap",
-        title: "Hello World!",
-      });
-    },
-  },
 };
