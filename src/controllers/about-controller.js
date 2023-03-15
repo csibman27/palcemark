290820b32cf17121155658b804d2ad2e180ec021
tibor molnar <tmolnair@gmail.com>
2023-03-15 20:40:16 +0000

    google maps to about page

--- src/controllers/about-controller.js
+++ src/controllers/about-controller.js
@@ -1,5 +1,9 @@
 import { db } from "../models/db.js";
 
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
 export const aboutController = {
   index: {
     handler: function (request, h) {
@@ -7,6 +11,21 @@ export const aboutController = {
         title: "About Placemark",
       };
       return h.view("about-view", viewData);
+    },
+  },
+  gmaps: {
+    handler: function initMap() {
+      const myLatLng = { lat: 52.2458777, lng: -7.138828 };
+      const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14,
+        center: myLatLng,
+      });
+      new google.maps.Marker({
+        position: myLatLng,
+        map,
+        src: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBSH-MEAYrP70lwxXkW0_-bt1jq9uXKtjg&callback=initMap",
+        title: "Hello World!",
+      });
     },
   },
 };
