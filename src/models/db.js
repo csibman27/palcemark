efbed19c392fe24593dd0ff64070172f907209dc
tibor molnar <tmolnair@gmail.com>
2023-03-18 15:41:32 +0000

    working on message store db

--- src/models/db.js
+++ src/models/db.js
@@ -1,19 +1,19 @@
 import { userMemStore } from "./mem/user-mem-store.js";
 import { placemarkMemStore } from "./mem/placemark-mem-store.js";
 import { stationMemStore } from "./mem/station-mem-store.js";
+import { messageMemStore } from "./mem/message-mem-store.js";
 
 import { userJsonStore } from "./json/user-json-store.js";
 import { placemarkJsonStore } from "./json/placemark-json-store.js";
 import { stationJsonStore } from "./json/station-json-store.js";
+import { messageJsonStore } from "./json/message-store.js";
 
-import { connectMongo } from "./mongo/connect.js";
 import { userMongoStore } from "./mongo/user-mongo-store.js";
-
 import { placemarkMongoStore } from "./mongo/placemark-mongo-store.js";
 import { stationMongoStore } from "./mongo/station-mongo-store.js";
-
 import { messageMongoStore } from "./mongo/message-mongo-store.js";
-import { messageJsonStore } from "./json/message-store.js";
+
+import { connectMongo } from "./mongo/connect.js";
 
 export const db = {
   userStore: null,
@@ -40,7 +40,7 @@ export const db = {
         this.userStore = userMemStore;
         this.placemarkStore = placemarkMemStore;
         this.stationStore = stationMemStore;
-        this.messageStore = messageMongoStore;
+        this.messageStore = messageMemStore;
     }
   },
 };
