036c43443ca17a6139bb93677b84fa58adcdb4de
tibor molnar <tmolnair@gmail.com>
2023-03-19 14:16:43 +0000

    fixing an error in server.js line 101

--- src/server.js
+++ src/server.js
@@ -47,58 +47,57 @@ async function init() {
 
 const server = Hapi.server({
   port: process.env.PORT || 3000,
-  });
+});
 
-  await server.register(Inert);
-  await server.register(Vision);
-  await server.register(Cookie);
-  await server.register(jwt);
+await server.register(Inert);
+await server.register(Vision);
+await server.register(Cookie);
+await server.register(jwt);
 
-  await server.register([
-    Inert,
-    Vision,
-    {
-      plugin: HapiSwagger,
-      options: swaggerOptions,
-    },
-  ]);
+await server.register([
+  Inert,
+  Vision,
+  {
+    plugin: HapiSwagger,
+    options: swaggerOptions,
+  },
+]);
 
-  server.validator(Joi);
+server.validator(Joi);
 
-  server.views({
-    engines: {
-      hbs: Handlebars,
-    },
-    relativeTo: __dirname,
-    path: "./views",
-    layoutPath: "./views/layouts",
-    partialsPath: "./views/partials",
-    layout: true,
-    isCached: false,
-  });
+server.views({
+  engines: {
+    hbs: Handlebars,
+  },
+  relativeTo: __dirname,
+  path: "./views",
+  layoutPath: "./views/layouts",
+  partialsPath: "./views/partials",
+  layout: true,
+  isCached: false,
+});
 
-  server.auth.strategy("session", "cookie", {
-    cookie: {
-      name: process.env.cookie_name,
-      password: process.env.cookie_password,
-      isSecure: false,
-    },
-    redirectTo: "/",
-    validate: accountsController.validate,
-  });
-  server.auth.strategy("jwt", "jwt", {
-    key: process.env.cookie_password,
-    validate: validate,
-    verifyOptions: { algorithms: ["HS256"] },
-  });
-  server.auth.default("session");
+server.auth.strategy("session", "cookie", {
+  cookie: {
+    name: process.env.cookie_name,
+    password: process.env.cookie_password,
+    isSecure: false,
+  },
+  redirectTo: "/",
+  validate: accountsController.validate,
+});
+server.auth.strategy("jwt", "jwt", {
+  key: process.env.cookie_password,
+  validate: validate,
+  verifyOptions: { algorithms: ["HS256"] },
+});
+server.auth.default("session");
 
-  db.init("mongo");
-  server.route(webRoutes);
-  server.route(apiRoutes);
-  await server.start();
-  console.log("Server running on %s", server.info.uri);
-}
+db.init("mongo");
+server.route(webRoutes);
+server.route(apiRoutes);
+await server.start();
+console.log("Server running on %s", server.info.uri);
 
 process.on("unhandledRejection", (err) => {
   console.log(err);
